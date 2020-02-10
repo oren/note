ad4b3fa10eb793d36f6b629419cc80d39114e141
Oren <orengolan@gmail.com>
2020-02-10 01:19:44 -0800

    misc

--- src/note.cr
+++ src/note.cr
@@ -1,10 +1,11 @@
 module Note
   VERSION = "0.1.0"
 
-	# Process.run(ENV["EDITOR"], args: {"myfile.txt"})
-
-	stdout = IO::Memory.new
-	stderr = IO::Memory.new
-
-	Process.run("vim", args: {"/tmp/myfile.txt"}, output: stdout, error: stderr)
+	Process.run(
+		ENV["EDITOR"],
+		args: {"myfile.txt"},
+		input: Process::Redirect::Inherit,
+		output: Process::Redirect::Inherit,
+		error: Process::Redirect::Inherit,
+	)
 end
