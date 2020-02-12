6e7b38052b1e83637277dbc2b15b70c44d64b167
Oren <orengolan@gmail.com>
2020-02-12 01:13:47 -0800

    misc

--- src/note.cr
+++ src/note.cr
@@ -2,9 +2,15 @@ module Note
   VERSION = "0.1.0"
 	puts ARGV
 
+	file = "/home/oren/m/notes"
+
+	if !ARGV.empty?
+		file = ARGV[0]
+	end
+
 	Process.run(
 		ENV["EDITOR"],
-		ARGV,
+		args: {file},
 		input: Process::Redirect::Inherit,
 		output: Process::Redirect::Inherit,
 		error: Process::Redirect::Inherit,
