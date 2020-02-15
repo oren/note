cb44a1deadddce95f5989f1359d84c5b3785ace4
Oren <orengolan@gmail.com>
2020-02-15 01:46:37 -0800

    ask for location on first usage

--- src/note.cr
+++ src/note.cr
@@ -1,45 +1,69 @@
 require "option_parser"
+require "system/user"
+require "json"
 
-module Note
+class Note
   VERSION = "0.1.0"
-	date = false
+	@date = false
+	@notes_file = "./notes"
+	@config_file = "#{Path.home}/.config/note/config.json"
 
-	option_parser = OptionParser.parse do |parser|
-		parser.banner = "Note is a notes taking app"
+	def run
+		option_parser = OptionParser.parse do |parser|
+			parser.banner = "Note is a notes taking app"
 
-		parser.on "-d", "--date", "Add current date to notes" do
-			date = true
+			parser.on "-d", "--date", "Add current date to notes" do
+				@date = true
+			end
+			parser.on "-v", "--version", "Show version" do
+				puts "version 1.0"
+				exit
+			end
+			parser.on "-h", "--help", "Show help" do
+				puts parser
+				exit
+			end
 		end
-		parser.on "-v", "--version", "Show version" do
-			puts "version 1.0"
-			exit
+
+
+		if File.exists?(@config_file)
+			config = File.read(@config_file)
+			con =  JSON.parse(config)
+
+			if con["note_location"]
+				@notes_file = con["note_location"].to_s
+			end
+		else
+			p "This is the first time you are running the app. Choose location for your notes"
+			user_input = gets
+			@notes_file = user_input.to_s
 		end
-		parser.on "-h", "--help", "Show help" do
-			puts parser
-			exit
+
+		open_vim
+
+		if !ARGV.empty?
+			@notes_file = ARGV[0]
+		end
+
+		# add current date to top of file
+		if @date
+			content = ""
+			if File.exists?(@notes_file)
+				content = File.read(@notes_file)
+			end
+			File.write(@notes_file, Time.local.to_s("%Y-%m-%d") + "\n\n" + content)
 		end
 	end
 
-	notes_file = "./notes"
-
-	if !ARGV.empty?
-		notes_file = ARGV[0]
+	def open_vim
+		Process.run(
+			ENV["EDITOR"],
+			args: {@notes_file},
+			input: Process::Redirect::Inherit,
+			output: Process::Redirect::Inherit,
+			error: Process::Redirect::Inherit,
+		)
 	end
-
-	# add current date to top of file
-	if date
-		content = ""
-		if File.exists?(notes_file)
-			content = File.read(notes_file)
-		end
-		File.write(notes_file, Time.local.to_s("%Y-%m-%d") + "\n\n" + content)
-	end
-
-	Process.run(
-		ENV["EDITOR"],
-		args: {notes_file},
-		input: Process::Redirect::Inherit,
-		output: Process::Redirect::Inherit,
-		error: Process::Redirect::Inherit,
-	)
 end
+
+Note.new.run
